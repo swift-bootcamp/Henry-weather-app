6ef49da2e3c42af519b59e5424541918fe3e4683
Henry <Henry@Henryteki-MacBook-Pro.local>
2014-08-16 14:23:56 +0800

    Interface Builder Outlet declare city and country: UILabel + !

--- weather app/ViewController.swift
+++ weather app/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var city: UILabel!
+    @IBOutlet var country: UILabel!
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
